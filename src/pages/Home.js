91355ede9bf1152a7bd6f9f51e0af22d88c37087
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2022-10-25 10:53:26 +0000

    [WIP ]Create Side bar Nav

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
 import React, {useState} from "react";
+import SidebarNav from "../components/SidebarNav";
 
 
 export default function Home() {
     const [count, setCount] = useState(0);
     return (
         <div>
-        <h1>Home</h1>
-        <p>Count: {count}</p>
+        <SidebarNav />
         <button onClick={() => setCount(count + 1)}>Increment</button>
         </div>
     );
