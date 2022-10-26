e14fdf163e9e80e0dc01a996df4b351b6cc77145
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-10-26 10:04:34 +0300

    Updated data

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -1,33 +1,33 @@
 import {
-    HomeIcon,
-    LayoutIcon,
-    CalendarIcon,
-    InvoiceIcon,
-    UserIcon,
-    RolesIcon,
-    PagesIcon,
-    AuthIcon,
-    WizardIcon,
-    ModalIcon,
-  } from "./Icons";
-  
-  export const SIDEBAR_DATA = [
-    {
-      id: 1,
-      name: "dashboards",
-      path: "dashboards",
-      icon: <HomeIcon />,
-    },
-    {
-      id: 2,
-      name: "Show Teachers",
-      path: "showteachers",
-      icon: <LayoutIcon />,
-    },
-    {
-        id: 3,
-        name: "Add a teacher",
-        path: "addteacher",
-        icon: <RolesIcon />,
-      },
-  ];
+  HomeIcon,
+  LayoutIcon,
+  CalendarIcon,
+  InvoiceIcon,
+  UserIcon,
+  RolesIcon,
+  PagesIcon,
+  AuthIcon,
+  WizardIcon,
+  ModalIcon,
+} from "./Icons";
+
+export const SIDEBAR_DATA = [
+  {
+    id: 1,
+    name: "dashboards",
+    path: "dashboards",
+    icon: <HomeIcon />,
+  },
+  {
+    id: 2,
+    name: "Reservations",
+    path: "showteachers",
+    icon: <LayoutIcon />,
+  },
+  {
+    id: 3,
+    name: "Book appointment",
+    path: "addteacher",
+    icon: <RolesIcon />,
+  },
+];
