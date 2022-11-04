6fa99b45ff4cb3ec0cff9eeae26c90e38f769c0c
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2022-11-04 09:10:27 +0000

    Switch to heroku Backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
-// const baseUrl = 'https://intense-headland-75088.herokuapp.com';
-const baseUrl = 'http://localhost:3001';
+const baseUrl = 'https://intense-headland-75088.herokuapp.com';
+// const baseUrl = 'http://localhost:3001';
 
 export const login = async ({ email, password }) => {
   try {
